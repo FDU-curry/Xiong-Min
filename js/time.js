0fcf3942b662f067162c17de952fb13d2196503c
FDU-curry <489374338@qq.com>
2020-03-17 21:28:37 +0800

    1

---
+++ js/time.js
@@ -0,0 +1,20 @@
+﻿function showTime() { 
+                var currentTimeString = "复旦大学 -- 罗杰 "
+	var currentDate = new Date(); 
+	var startDate = new Date(2014, 01, 01);
+	var date3 = currentDate-startDate;
+	var days=Math.floor(date3/(24*3600*1000));
+	var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
+	var hours=Math.floor(leave1/(3600*1000));
+	var leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
+	var minutes=Math.floor(leave2/(60*1000));
+	var leave3=leave2%(60*1000);          //计算分钟数后剩余的毫秒数
+	var seconds=Math.round(leave3/1000);
+	if (minutes < 10) 
+	minutes = "0" + minutes; 
+	if (seconds < 10) 
+	seconds = "0" + seconds; 
+                var currentTimeString = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp复旦大学 -- 罗杰 <br>"; 
+	document.getElementById("show").innerHTML=currentTimeString;  //改这地方
+	window.setTimeout("showTime()", 1000); 
+} 
